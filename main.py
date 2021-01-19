c21aecaf77fd0b941700c7961a9b86d1b9513b64
hyperchessbot <hyperchessbot@gmail.com>
2021-01-19 07:53:22 +0000

    Cargo.roml

--- main.py
+++ main.py
@@ -36,20 +36,53 @@ for filepath in glob.iglob('crates/*.toml'):
   root = f"generated/{name}"
   print(root)
   create_dirs(root)
+  authors = all_config.get("author", [{
+    "name": "Joe Doe",
+    "email": "joedoe@gmail.com",
+  }])
+  main_author = authors[0]
+  vcss = all_config.get("vcs", [{
+    "type": "github",
+    "username": "joedoe",
+    "useremail": main_author["email"],
+  }])
+  main_vcs = vcss[0]  
+  version = all_config.get("version", "0.1.0")
+  edition = all_config.get("edition", "2018")
+  description = all_config.get("description", f"Crate {name}.")
+  license = all_config.get("license", "MIT")
+  keywords = all_config.get("keywords", ["crate", name])
+
+  repo = f"https://{main_vcs['type']}.com/{main_vcs['username']}/{name}"
+  repository = all_config.get("repository", repo)
+  homepage = all_config.get("homepage", f"{repo}#{name}")
+  documentation = all_config.get("documentation", f"https://docs.rs/{name}/{version}")
+  readme = all_config.get("readme", "ReadMe.md")
+
   package = {
     "name": name,
-    "authors": [f"{author['name']} <{author['email']}>" for author in global_config["author"]],
-    "keywords": all_config["keywords"]
+    "version": version,
+    "authors": [f"{author['name']} <{author['email']}>" for author in authors],
+    "edition": edition,
+    "keywords": keywords,
+    "description": description,
+    "license": license,
+    "repository": repository,
+    "homepage": homepage,
+    "documentation": documentation,
+    "readme": readme,
   }
-  bbin = {
+  bbin = all_config.get("bin", [{
     "name": "example",
     "path": "src/example.rs"
-  }
+  }])
+  dependencies = all_config.get("dependencies", {"dotenv": "0.15.0", "log": "0.4.11"})
   cargo_toml = {
     "package": package,    
-    #"bin": bbin
+    "dependencies": dependencies,
+    "bin": bbin,
+    "lib": {"path": "src/lib.rs"}
   }
-  print(cargo_toml)
-  dump_json(f"{root}/Cargo.json", cargo_toml)
+  print(cargo_toml)  
   dump_toml(f"{root}/Cargo.toml", cargo_toml)
   
