95763666bc2d1f1b89570e850b178b9f98e897ac
hyperchessbot <hyperchessbot@gmail.com>
2021-01-19 06:32:02 +0000

    Merge configs

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import toml
+import glob
 
 global_config = toml.load("global.toml")
 
 print(global_config)
+
+for filepath in glob.iglob('crates/*.toml'):
+    print(filepath)
+    crate_config = toml.load(filepath)
+    print(crate_config)
+    all_config = {**global_config, **crate_config}
+    print(all_config)
